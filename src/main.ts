98439ffff6b134fb9dc0680f36f1ba8180aed8b3
maphe <sokaru91@gmail.com>
2020-02-25 22:47:00 -0800

    Heroku setting

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
